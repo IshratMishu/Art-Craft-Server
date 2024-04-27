2e131838fd56abab426a983fbd4ea2ac08b0f508
IshratMishu <ishratjahan0425@gmail.com>
2024-04-28 00:50:38 +0600

    await error fixed

--- index.js
+++ index.js
@@ -45,8 +45,6 @@ async function run() {
 
 
 
-
-
     app.get("/addList/:email", async (req, res) => {
       console.log(req.params.email);
       const result = await potteryCollection.find({ email: req.params.email }).toArray();
@@ -65,22 +63,15 @@ async function run() {
 
 
 
-
-
-
-
-
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    // await client.close();
+    await client.close();
   }
 }
 run().catch(console.dir);
-
-
 
 
 
